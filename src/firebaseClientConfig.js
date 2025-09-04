3c58d9fcf3f32db6b393d169ea605ce19b9f4bb1
anabmguimaraes <anabm.guimaraes@gmail.com>
2025-09-04 11:13:01 -0300

    Update firebaseClientConfig.js

--- src/firebaseClientConfig.js
+++ src/firebaseClientConfig.js
@@ -2,14 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // Importa o serviço de Storage
+import { getStorage } from "firebase/storage";
 
 // A configuração do seu projeto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCNU5ZEl60OcW5eZyL_ZoD0tFKpweQvhwU",
   authDomain: "crmdoceria-9959e.firebaseapp.com",
   projectId: "crmdoceria-9959e",
-  storageBucket: "crmdoceria-9959e.appspot.com",
+  // CORREÇÃO APLICADA AQUI
+  storageBucket: "crmdoceria-9959e.firebasestorage.app",
   messagingSenderId: "389481198252",
   appId: "1:389481198252:web:429bff3cc5d4f353bea509",
   measurementId: "G-XJ7LPG0229"
@@ -21,5 +22,5 @@ const app = initializeApp(firebaseConfig);
 // Exporte os serviços que você usará na sua aplicação
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app); // Exporta o serviço de Storage
+export const storage = getStorage(app);
 
